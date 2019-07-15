6a846dddc551ffeae5c3d64e38a856296bf77321
togoboy8 <cedrichillah@gmail.com>
2019-07-15 15:55:52 -0400

    first commit

---
+++ src/main/java/com/example/demo/SprintBoot2041Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBoot2041Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBoot2041Application.class, args);
+	}
+
+}
